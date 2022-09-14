189763ccabfd357cfba37958e40d1154f731f770
rahulghoshal <rahulghoshal482@gmail.com>
2022-09-14 17:35:50 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [count,setCount]=useState(0);
+  
+  const handleAdd=()=>{
+    setCount(count+1);
+  }
+  const handleDec=()=>{
+    if(count>0){
+      setCount(count-1);
+    }
+   
+  }
+  const handleReset=()=>{
+    setCount(0);
+  }
+
   return (
+     <View style={styles.parentcontainer}>
+   <Text style={styles.heading}> My CounterApp</Text>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}> Counter: {count}</Text><br/>
+      <TouchableOpacity style={styles.button} onPress={handleAdd} >
+      <Text style={styles.buttonText}>Increment</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button2} onPress={handleDec} >
+      <Text style={styles.buttonText}>Decrement</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button3} onPress={handleReset} >
+      <Text style={styles.buttonText}>Reset</Text>
+      </TouchableOpacity>
+    </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  parentcontainer:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "auto",
+    backgroundColor:"grey",
   },
+  heading:{
+    fontSize:30,
+    color:"white",
+    marginTop:20
+  },
+  container:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "auto",
+    backgroundColor:"grey",
+  },
+  text:{
+    fontSize:30,
+    color:"white"
+  },
+  button:{
+    width:100,
+    backgroundColor:"green",
+    padding:10,
+    borderRadius:5,
+    marginRight:200
+  },
+  buttonText:{
+    fontSize:15,
+    color:'white'
+    
+  },
+  button2:{
+    width:100,
+    backgroundColor:"red",
+    padding:10,
+    borderRadius:5,
+     marginBottom:200,
+    float:"right",
+     marginLeft:20,
+     marginTop:-40
+  
+  },
+  button3:{
+    width:80,
+    backgroundColor:"orange",
+    padding:10,
+    borderRadius:5,
+   marginBottom:200,
+    float:"right",
+     marginLeft:250,
+     marginTop:-240
+   
+  },
+  
+ 
+  
 });
+
+export default App;
